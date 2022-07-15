684e84b2bdb2c544ef10eacff763c88214d3625b
Łukasz Bijak <105939450+LukaszBijak@users.noreply.github.com>
2022-07-15 12:14:32 +0200

    coffe machine in OOP

---
+++ day-16-OOP.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color('blue', 'coral')
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+
+my_screen =Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+timmy.color('red', 'green')
+
